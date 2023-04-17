ce3b02693f5f2d1574ef8fdb1e2d5614738716d7
David Gabriel <davidg2642@gmail.com>
2023-04-17 18:30:34 -0300

    clase RondaTest creada

--- src/main/java/ap40/c147/tpg9/Ronda.java
+++ src/main/java/ap40/c147/tpg9/Ronda.java
@@ -19,7 +19,18 @@ public class Ronda {
     // Lista de partidos de la ronda
     private ArrayList<Partido> partidos;
 
-    public Ronda(int id, String nro, ArrayList<Partido> partidos) throws RondaException{
+    /**
+     *
+     * Constructor para crear una instancia de Ronda
+     *
+     * @param id identificador unico de la ronda. Debe ser un valor positivo
+     * distinto de cero
+     * @param nro numero de la ronda. No puede estar vacio
+     * @param partidos lista de partidos asociados a la ronda. No puede ser nula
+     * @throws ap40.c147.exceptions.RondaException si alguno de los argumentos
+     * es nulo o invalido
+     */
+    public Ronda(int id, String nro, ArrayList<Partido> partidos) throws RondaException {
 
         if (id <= 0) {
             throw new RondaException("El id no puede ser un numero cero o menor a cero");
@@ -36,6 +47,36 @@ public class Ronda {
         this.id = id;
         this.nro = nro;
         this.partidos = partidos;
+    }
+
+    /**
+     *
+     * Obtiene un partido de la lista de partidos de la ronda, dado su
+     * identificador.
+     *
+     * @param partidoId identificador único del partido a buscar.
+     * @return Partido encontrado o null si no se encuentra el partido con el
+     * identificador dado.
+     * @throws ap40.c147.exceptions.RondaException si alguno de los argumentos
+     * es nulo o invalido
+     */
+    public Partido obtenerPartido(int partidoId) throws RondaException {
+
+        if (partidoId <= 0) {
+            throw new RondaException("El id no puede ser un numero cero o menor a cero");
+        }
+
+        Partido partidoBuscado = null;
+
+        for (Partido partido : partidos) {
+            if (partido.getId() == partidoId) {
+                partidoBuscado = partido;
+                break;
+            }
+        }
+
+        return partidoBuscado;
+
     }
 
 }
---
+++ src/test/java/ap40/c147/tpg9/RondaTest.java
@@ -0,0 +1,84 @@
+package ap40.c147.tpg9;
+
+import ap40.c147.exceptions.RondaException;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author david
+ */
+public class RondaTest {
+
+    @Test
+    public void constructorRondaCorrecto() throws RondaException {
+
+        ArrayList<Partido> partidos = new ArrayList<>();
+        Ronda ronda = new Ronda(1, "Ronda 1", partidos);
+
+        Assertions.assertEquals(1, ronda.getId());
+        Assertions.assertEquals("Ronda 1", ronda.getNro());
+        Assertions.assertEquals(partidos, ronda.getPartidos());
+
+    }
+
+    @Test
+    public void constructorRondaIncorrecto() {
+
+        ArrayList<Partido> partidos = new ArrayList<>();
+
+        Assertions.assertThrows(RondaException.class, () -> new Ronda(0, "Ronda 1", partidos));
+        Assertions.assertThrows(RondaException.class, () -> new Ronda(1, "", partidos));
+        Assertions.assertThrows(RondaException.class, () -> new Ronda(0, "Ronda 1", null));
+    }
+
+    @Test
+    public void obtenerPartidoEncuentraPartido() throws RondaException {
+
+        Equipo equipo1 = new Equipo();
+        Equipo equipo2 = new Equipo();
+        Partido partido = new Partido(1, equipo1, equipo2, 2, 0);
+
+        ArrayList<Partido> partidos = new ArrayList<>();
+
+        partidos.add(partido);
+
+        Ronda ronda = new Ronda(1, "Numero 1", partidos);
+
+        Partido partidoEncontrado = ronda.obtenerPartido(1);
+
+        Assertions.assertNotNull(partidoEncontrado);
+
+    }
+
+    @Test
+    public void obtenerPartidoNoEncuentraPartido() throws Exception {
+        Equipo equipo1 = new Equipo();
+        Equipo equipo2 = new Equipo();
+        Partido partido = new Partido(1, equipo1, equipo2, 2, 1);
+
+        ArrayList<Partido> partidos = new ArrayList<>();
+        partidos.add(partido);
+
+        Ronda ronda = new Ronda(1, "Ronda 1", partidos);
+
+        Assertions.assertNull(ronda.obtenerPartido(2));
+    }
+
+    @Test
+    public void obtenerPartidoArgumentoInvalido() throws RondaException {
+        Equipo equipo1 = new Equipo();
+        Equipo equipo2 = new Equipo();
+        Partido partido = new Partido(1, equipo1, equipo2, 2, 1);
+
+        ArrayList<Partido> partidos = new ArrayList<>();
+        partidos.add(partido);
+
+        Ronda ronda = new Ronda(1, "Ronda 1", partidos);
+
+        Assertions.assertThrows(RondaException.class, () -> ronda.obtenerPartido(0));
+
+    }
+
+}
