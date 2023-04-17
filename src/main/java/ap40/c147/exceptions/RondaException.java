1bdf3ebc609521159fcbc9d8a35692505831846c
David Gabriel <davidg2642@gmail.com>
2023-04-17 15:15:12 -0300

    creando la excepcion para la clase Ronda

---
+++ src/main/java/ap40/c147/exceptions/RondaException.java
@@ -0,0 +1,14 @@
+package ap40.c147.exceptions;
+
+/**
+ * Excepcion personalizada para la clase Ronda
+ * @author david
+ */
+public class RondaException extends Exception{
+
+    public RondaException(String message) {
+        super(message);
+    }
+    
+    
+}
