8726189d3eb34195a14fbd93a7a0306fcda56296
David Gabriel <davidg2642@gmail.com>
2023-04-23 14:33:58 -0300

    Clase Participante

---
+++ src/main/java/ap40/c147/tpg9/Participante.java
@@ -0,0 +1,22 @@
+package ap40.c147.tpg9;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author david
+ */
+@Setter
+@Getter
+public class Participante {
+
+    private int id;
+    private String nombre;
+
+    public Participante(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+    
+}
